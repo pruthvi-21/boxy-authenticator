1c866a50b4c97cc1810b36c5fc47da9e3c55b12b
Pruthvi Bugidi <github.skid124@slmail.me>
2025-06-09 21:06:50 +0530

    refactor: use core viewModel dsl for compose 1.6 compatibility

--- composeApp/src/commonMain/kotlin/com/boxy/authenticator/di/KoinModule.kt
+++ composeApp/src/commonMain/kotlin/com/boxy/authenticator/di/KoinModule.kt
@@ -1,6 +1,8 @@
 package com.boxy.authenticator.di
 
 import com.boxy.authenticator.core.BiometricsHelper
+import com.boxy.authenticator.core.SettingsDataStore
+import com.boxy.authenticator.core.TokenFormValidator
 import com.boxy.authenticator.data.database.DatabaseDriverFactory
 import com.boxy.authenticator.data.database.dao.LocalTokenDao
 import com.boxy.authenticator.data.database.repository.LocalTokenRepository
@@ -10,15 +12,13 @@ import com.boxy.authenticator.domain.database.repository.TokenRepository
 import com.boxy.authenticator.domain.usecases.DeleteTokenUseCase
 import com.boxy.authenticator.domain.usecases.FetchTokenByIdUseCase
 import com.boxy.authenticator.domain.usecases.FetchTokenByNameUseCase
+import com.boxy.authenticator.domain.usecases.FetchTokenCountUseCase
 import com.boxy.authenticator.domain.usecases.FetchTokensUseCase
 import com.boxy.authenticator.domain.usecases.InsertTokenUseCase
 import com.boxy.authenticator.domain.usecases.InsertTokensUseCase
 import com.boxy.authenticator.domain.usecases.ReplaceExistingTokenUseCase
-import com.boxy.authenticator.domain.usecases.UpdateTokenUseCase
-import com.boxy.authenticator.core.SettingsDataStore
-import com.boxy.authenticator.core.TokenFormValidator
-import com.boxy.authenticator.domain.usecases.FetchTokenCountUseCase
 import com.boxy.authenticator.domain.usecases.UpdateHotpCounterUseCase
+import com.boxy.authenticator.domain.usecases.UpdateTokenUseCase
 import com.boxy.authenticator.ui.viewmodels.AuthenticationViewModel
 import com.boxy.authenticator.ui.viewmodels.ExportTokensViewModel
 import com.boxy.authenticator.ui.viewmodels.HomeViewModel
@@ -26,8 +26,8 @@ import com.boxy.authenticator.ui.viewmodels.ImportTokensViewModel
 import com.boxy.authenticator.ui.viewmodels.SettingsViewModel
 import com.boxy.authenticator.ui.viewmodels.TokenSetupViewModel
 import dev.icerock.moko.biometry.BiometryAuthenticator
-import org.koin.compose.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 expect val platformModule: Module
